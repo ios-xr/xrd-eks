556d444199c4b586ac177ac6674102d09dd2f072
Tom Johnes <tjohnes@cisco.com>
2023-02-06 23:02:14 +0000

    Add KubernetesVersion as a type

--- test/_types.py
+++ test/_types.py
@@ -1,7 +1,25 @@
+# _types.py
+
+__all__ = (
+    "Image",
+    "Kubectl",
+    "KubernetesVersion",
+    "Platform",
+)
+
 import dataclasses
 import enum
 import subprocess
 from typing import Callable
+
+
+class KubernetesVersion(str, enum.Enum):
+    V1_22 = "1.22"
+    V1_23 = "1.23"
+    V1_24 = "1.24"
+
+    def __str__(self):
+        return self.value
 
 
 class Platform(str, enum.Enum):
